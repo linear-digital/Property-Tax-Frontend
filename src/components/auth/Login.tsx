288b1f453461dabe20b1952ac794a92bf67905a3
Linear Digital (Tamiz) <115143466+linear-digital@users.noreply.github.com>
2025-08-28 14:24:47 +0600

    Update Login.tsx

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -55,7 +55,7 @@ const Login = () => {
                     className='w-[100px] h-[100px] mx-auto object-cover rounded-full'
                 />
                 <h2 className="text-white text-center text-xl mt-2">{branch?.name}</h2>
-                <p className="text-sm text-white mt-1 mb-2 text-center">{branch?.description} </p>
+                <p className="text-sm text-white mb-2 text-center">{branch?.description} </p>
                 <Input
                     label='Email Address'
                     type='email'
